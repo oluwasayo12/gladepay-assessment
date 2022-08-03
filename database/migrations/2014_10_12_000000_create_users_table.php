585f77e63fce6c34ee25ca3a60cfb2b6d6cc0aee
Oluwasayo Oyedepo <olusayo@Users-MacBook-Pro.local>
2022-08-03 01:54:39 +0100

    database issue fix -- resolved

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,7 +17,7 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('email',150)->unique();
+            $table->string('email')->unique();
             $table->string('password');
             $table->boolean('status')->default(1);
             $table->rememberToken();
